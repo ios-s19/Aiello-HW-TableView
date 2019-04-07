9ad2c4aaaada7a0477526fdc7e3023b2a9d129d9
Tony Aiello <aiell016@rangers.uwp.edu>
2019-04-06 22:05:11 -0500

    Added framework  for UITableView (chapter 10)

---
+++ Aiello-HW-TableView/Item.swift
@@ -0,0 +1,47 @@
+//
+//  Item.swift
+//  Aiello-HW-TableView
+//
+//  Created by Tony Aiello on 4/6/19.
+//  Copyright © 2019 Tony Aiello. All rights reserved.
+//
+
+
+
+
+
+import UIKit
+
+
+
+class Item: NSObject {
+    var name: String
+    var serialNumber: String?
+    
+    init(name: String, serialNumber: String?) {
+        
+        self.name = name
+        self.serialNumber = serialNumber
+        
+        super.init()
+        
+    }
+    
+        
+        convenience init(random: Bool = false) {
+            if random {
+                let names = ["Buckingham Palace", "The Eiffel Tower", "The Grand Canyon", "Windsor Castle", "Empire State Buolding"]
+                var idx = arc4random_uniform(UInt32(names.count))
+                let randomName = names[Int(idx)]
+                let randomSerialNumber = UUID().uuidString.components(separatedBy: "-").first!
+                
+                self.init(name: randomName, serialNumber: randomSerialNumber)
+            } else {
+                self.init(name: "", serialNumber: nil)
+                
+            }
+        }
+        
+    
+    
+}
---
+++ Aiello-HW-TableView/ItemsViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ItemsViewController.swift
+//  Aiello-HW-TableView
+//
+//  Created by Tony Aiello on 4/6/19.
+//  Copyright © 2019 Tony Aiello. All rights reserved.
+//
+
+
+
+import UIKit
+
+class ItemsViewController: UITableViewController {
+    
+    
+}
