0d9a851760692ddf8ac698849437ea3aeb434ef1
Tony Aiello <aiell016@rangers.uwp.edu>
2019-04-06 23:03:03 -0500

    Beginning to add photo element to cell

--- Aiello-HW-TableView/Item.swift
+++ Aiello-HW-TableView/Item.swift
@@ -31,6 +31,8 @@ class Item: NSObject {
         convenience init(random: Bool = false) {
             if random {
                 let names = ["Buckingham Palace", "The Eiffel Tower", "The Grand Canyon", "Windsor Castle", "Empire State Buolding"]
+                let photos = ["palace.jpg", "tower.jpg", "canyon.jpg", "castle.jpg", "building.jpg"]
+                
                 var idx = arc4random_uniform(UInt32(names.count))
                 let randomName = names[Int(idx)]
                 let randomSerialNumber = UUID().uuidString.components(separatedBy: "-").first!
